4d30b5ed7a9e3b23f89f0b9e4fb1e56a15c93f2d
somagouda <madagoudasomagouda@gmail.com>
2026-05-18 17:04:42 +0530

    timeWaste

---
+++ script.js
@@ -0,0 +1,78 @@
+//   // function solve(){
+//   //   var age=20;
+//   //   console.log(age);
+
+//   // }
+//   // solve();
+//   // console.log(age);
+//   var x=10;
+//   console.log(x);
+//   var x=20;
+//   console.log(x);
+//   let a=10;
+//   console.log(a);
+// a=20;
+// console.log(a);
+// console.log(typeof(a));
+// let m="somu";
+// console.log(m);
+// console.log(typeof(m));
+// m=true;
+// console.log(m);
+// console.log(typeof(m));
+// function get(a,b){
+//   return a*b;
+// }
+// console.log(get(10,20));
+let obj={
+  name:"somu",
+  "full name":"s s"
+};
+// console.log(ob);
+// console.log(ob.name);
+// let arr=[1,2,3,4,5];
+// console.log(arr);
+// let brr=new Array(1,2,3,'true');
+// console.log(brr);
+// console.log(typeof(brr));
+// for(let i=0;i<10;i++){
+//   brr.push(i);
+// }
+// console.log(brr);
+// brr.pop();
+// console.log(brr);
+// brr.shift();
+// console.log(brr);   
+// brr.unshift("somu");
+// console.log(brr); 
+// console.log(brr.slice (2,4));
+// let ar=[10,20,30,40];
+// // ar.map((number)=>{
+// //  console.log (number+1);
+// // })
+// let ans=ar.reduce((acc,cur)=>{
+//   return acc+cur;
+// });
+// console.log(ans);
+// ar.sort();
+// console.log(ar);
+// console.log(ar.indexOf(40 ));
+// let sol=ar.find(x=> x>30);
+// console.log(sol);
+// ar.forEach((value,index)=>{
+//   console.log("Number:",value,"Index",index);
+// })
+// ar.forEach((index)=>{
+//   console.log(index);
+// })
+// for (key in obj){
+//   console.log(key);
+// }
+
+
+
+hello();
+function hello(){
+  console.log("hello");
+}
+hello();
